5c3c29728491024658c3e28275b04fb4e8feb7a1
Anast001 <partner4designer1997@gmail.com>
2024-05-12 20:07:19 +0530

    ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,13 @@ import "@/styles/globals.css"
 import { Metadata } from "next"
 
 import { siteConfig } from "@/config/site"
-import { fontSans } from "@/lib/fonts"
+import { Inter } from 'next/font/google'
 import { cn } from "@/lib/utils"
 // import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
+
+
+const inter = Inter({ subsets: ['latin'], display: 'swap', adjustFontFallback: false})
 
 export const metadata: Metadata = {
   title: {
@@ -34,10 +37,11 @@ export default function RootLayout({ children }: RootLayoutProps) {
       <html lang="en" suppressHydrationWarning>
         <head />
         <body
-          className={cn(
-            "min-h-screen bg-background font-sans antialiased",
-            fontSans.variable
-          )}
+        className={inter.className}
+          // className={cn(
+          //   "min-h-screen bg-background font-sans antialiased",
+           
+          // )}
         >
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             <div className="relative flex min-h-screen flex-col">
